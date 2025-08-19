a5077441c2c547801cef326380698091d9b669d8
Anri Begheladze <anriskr14@gmail.com>
2025-08-19 16:55:58 +0400

    changed visuals , added light-dark mode , added checkbox

--- script.js
+++ script.js
@@ -1,35 +1,109 @@
-let input = document.getElementById('input')
-const addBtn = document.querySelector('.addBtn')
-const ul = document.getElementById('ul')
+let input = document.getElementById("input");
+const addBtn = document.querySelector(".addBtn");
+const ul = document.getElementById("ul");
+const colorChangeBtn = document.querySelector('.light-dark')
 
-addBtn.addEventListener('click' , () => {
-  const li = document.createElement('li')
-  let span = document.createElement('span')
-  const deleteBtn = document.createElement('button')
+
+
+
+colorChangeBtn.addEventListener('click' , () => {
+  const body = document.body
+   body.classList.toggle('background-dark')
+  if (  body.classList.contains('background-dark')) {
+        colorChangeBtn.textContent = 'Dark Mode'
+        colorChangeBtn.classList.add('btn-dark')
+        colorChangeBtn.classList.rmeove('btn-light')
+
+
+  } else  {
+    colorChangeBtn.textContent = 'Light Mode'
+     colorChangeBtn.classList.remove('btn-dark')
+     colorChangeBtn.classList.add('btn-light')
+  }
+
+ 
+
+
+})
+
+
+
+addBtn.addEventListener("click", () => {
+  const li = document.createElement("li");
+  const label = document.createElement('label')
+  const checkbox = document.createElement('input')
+  let span = document.createElement("span");
+  const deleteBtn = document.createElement("button");
+  
+
+// giving class names 
+checkbox.className = 'checkbox';
+checkbox.type = 'checkbox';
+label.className = 'label';
+span.className = 'list-text';
+deleteBtn.className = "delete-btn";
+deleteBtn.textContent = "X";
+
+
 
   if (input.value.trim() === "") {
-  alert('Please write something')
-  return;
-}
-
-deleteBtn.className = "delete-btn"
-
- span.textContent = input.value
- deleteBtn.textContent = 'X'
- input.value = ""
-
- deleteBtn.addEventListener('click' , () => {
-  li.remove()
- })
-
-li.appendChild(span)
-ul.appendChild(deleteBtn)
-ul.appendChild(li)
-
-})
-
-input.addEventListener("keydown" , (e) => {
-  if(e.key === "Enter") {
-    addBtn.click()
+    alert("Please write something");
+    return;
   }
-})
+
+
+
+  span.textContent = input.value;
+  
+  input.value = "";
+
+  
+  label.appendChild(checkbox)
+  label.appendChild(span)
+  li.appendChild(label)
+  li.appendChild(deleteBtn)
+  ul.appendChild(li)
+
+
+ 
+
+
+  checkbox.addEventListener('change' , () => {
+    if (checkbox.checked) {
+      deleteBtn.textContent = '✔'
+      deleteBtn.style.color = "green"; 
+    
+    } else {
+      deleteBtn.textContent = 'X'
+      deleteBtn.style.color = "rgb(250, 10, 10)"; 
+      
+
+      
+    }
+ 
+    
+  })
+
+
+
+  deleteBtn.addEventListener("click", () => {
+    li.remove();
+   
+  
+
+
+  });
+});
+
+input.addEventListener("keydown", (e) => {
+  if (e.key === "Enter") {
+    addBtn.click();
+  }
+
+
+
+
+
+});
+
+
